a67d68b2d5d414c9de1543c085bca464dd7bff47
100111100 <253274870@qq.com>
2019-08-05 09:47:15 +0800

    修改方法名称

--- ibr-scheduler-svr/src/main/java/rebue/ibr/scheduler/task/IbrOrderSettleTasks.java
+++ ibr-scheduler-svr/src/main/java/rebue/ibr/scheduler/task/IbrOrderSettleTasks.java
@@ -1,7 +1,5 @@
 package rebue.ibr.scheduler.task;
 
-import java.util.List;
-
 import javax.annotation.Resource;
 
 import org.slf4j.Logger;
@@ -9,9 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import rebue.ibr.dic.TaskTypeDic;
 import rebue.ibr.svr.feign.IbrBuyRelationTaskSvc;
-import rebue.robotech.dic.TaskExecuteStateDic;
 
 /**
  * 订单结算任务
@@ -27,22 +23,21 @@ public class IbrOrderSettleTasks {
     // settleRelation:订单结算执行的间隔(毫秒)，默认5分钟检查一次
     @Scheduled(fixedDelayString = "${ibr.scheduler.settleRelation:300000}")
     public void executeTasks() throws InterruptedException {
-        _log.info("定时执行需要订单结算的的任务");
-        List<Long> taskIds = ibrBuyRelationTaskSvc.getTaskIdsThatShouldExecute(TaskExecuteStateDic.NONE,
-                TaskTypeDic.SETTLE_COMMISSION);
-        _log.info("获取到所有需要订单结算的任务的列表为：{}", taskIds);
-        try {
-            for (Long taskId : taskIds) {
-                try {
-                    _log.info("当前任务id-{}", taskId);
-                    ibrBuyRelationTaskSvc.executeOrderBuyRelationTask(taskId);
-                    Thread.sleep(1000);
-                } catch (final RuntimeException e) {
-                    _log.error("需要执行订单结算任务失败", e);
-                }
-            }
-        } catch (final RuntimeException e) {
-            _log.info("获取需要执行订单结算任务时出现异常", e);
-        }
+//        _log.info("定时执行需要订单结算的的任务");
+//        List<Long> taskIds = ibrBuyRelationTaskSvc.getTaskIdsThatShouldSettleExecute(TaskExecuteStateDic.NONE);
+//        _log.info("获取到所有需要订单结算的任务的列表为：{}", taskIds);
+//        try {
+//            for (Long taskId : taskIds) {
+//                try {
+//                    _log.info("当前任务id-{}", taskId);
+//                    ibrBuyRelationTaskSvc.executeOrderSettleTask(taskId);
+//                    Thread.sleep(1000);
+//                } catch (final RuntimeException e) {
+//                    _log.error("需要执行订单结算任务失败", e);
+//                }
+//            }
+//        } catch (final RuntimeException e) {
+//            _log.info("获取需要执行订单结算任务时出现异常", e);
+//        }
     }
 }
